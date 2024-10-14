cca94f645291359e6e96d15957de6ad8f33f99c0
thanhhuynh1903 <thanhgaming1234@gmail.com>
2024-10-14 23:41:29 +0700

    update blog

--- src/pages/BlogDetail/BlogDetail.jsx
+++ src/pages/BlogDetail/BlogDetail.jsx
@@ -5,143 +5,140 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useParams } from 'react-router';
-import { useState,useEffect } from 'react';
-import { aget } from 'utils/util_axios';
-import { width } from '@mui/system';
-import { Description } from '@mui/icons-material';
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme }) => ({
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-  variants: [
-    {
-      props: ({ expand }) => !expand,
-      style: {
-        transform: 'rotate(0deg)',
-      },
-    },
-    {
-      props: ({ expand }) => !!expand,
-      style: {
-        transform: 'rotate(180deg)',
-      },
-    },
-  ],
-}));
+import { useState, useEffect } from 'react';
+import { aget, aupdate } from 'utils/util_axios';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export default function BlogDetail() {
-  const [expanded, setExpanded] = React.useState(false);
-
   const { id } = useParams();
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
+  const [editMode, setEditMode] = useState(false);
+  const [updatedData, setUpdatedData] = useState({ title: '', content: '', picture: '' });
 
   useEffect(() => {
     aget(`/blog/${id}`).then((res) => {
       setData(res.data);
+      setUpdatedData({ title: res.data.title, content: res.data.content, picture: res.data.picture });
     });
-  }, []);
-console.log(data);
-const formatDate = (timestamp) => {
+  }, [id]);
+
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const response = await aupdate(`/blog/${id}`, updatedData);
+      setData(response.data);
+      setEditMode(false);
+    } catch (error) {
+      console.error("Error updating blog post", error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUpdatedData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const formatDate = (timestamp) => {
     const date = new Date(timestamp);
     return date.toLocaleDateString();
   };
 
   return (
-    <div style={{width:"100%"}}>
-    <Card sx={{ maxWidth: "700px",margin: "0 auto" }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={data.authorId}
-        subheader={formatDate(data.createdAt) + " " +"Update at :" + " " + formatDate(data.updatedAt)}
-      />
-      <Typography sx={{marginLeft:2,marginY:1}}>
-        {data.title}
-      </Typography>
-      <CardMedia
-        component="img"
-        height="194"
-        image={data.picture}
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          <strong>Description : </strong>{data.content}
+    <div style={{ width: "100%" }}>
+      <Card sx={{ maxWidth: "700px", margin: "0 auto" }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          
+          title={data.authorId}
+          subheader={formatDate(data.createdAt) + " " + "Update at :" + " " + formatDate(data.updatedAt)}
+        />
+        <Typography sx={{ marginLeft: 2, marginY: 1 }}>
+          {data.title}
         </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <EditNoteIcon  />
-        </IconButton>
-        <IconButton aria-label="share">
-          <DeleteForeverIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardMedia
+          component="img"
+          height="194"
+          image={data.picture}
+          alt="Blog post image"
+        />
         <CardContent>
-          <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
-          <Typography sx={{ marginBottom: 2 }}>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography sx={{ marginBottom: 2 }}>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography sx={{ marginBottom: 2 }}>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          <Typography variant="body2" sx={{ color: 'text.secondary' ,fontSize:"18px"}}>
+            <strong>Description : </strong>{data.content}
           </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
+        <CardActions disableSpacing>
+          <IconButton aria-label="edit" onClick={handleEditClick}>
+            <EditNoteIcon />
+          </IconButton>
+          <IconButton aria-label="delete">
+            <DeleteForeverIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+
+      <Dialog open={editMode} onClose={() => setEditMode(false)}>
+        <DialogTitle>Edit Blog Post</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            name="title"
+            label="Title"
+            type="text"
+            fullWidth
+            variant="outlined"
+            value={updatedData.title}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="dense"
+            name="content"
+            label="Content"
+            type="text"
+            fullWidth
+            variant="outlined"
+            value={updatedData.content}
+            onChange={handleChange}
+            sx={{fontSize:"16px"}}
+          />
+          <TextField
+            margin="dense"
+            name="picture"
+            label="Picture URL"
+            type="text"
+            fullWidth
+            variant="outlined"
+            value={updatedData.picture}
+            onChange={handleChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setEditMode(false)}>Cancel</Button>
+          <Button onClick={handleUpdate}>Update</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
